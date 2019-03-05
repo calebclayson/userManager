b549d8e26615108b0ff406f87b5d45d24c9cfcd3
calebclayson <clayson.caleb@gmail.com>
2019-03-04 18:40:13 -0700

    created some views and basic form

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users');
+});
+
+router.get('/created', (req, res, next) => {
+  res.render('created', {
+    data: res
+  });
 });
 
 module.exports = router;
